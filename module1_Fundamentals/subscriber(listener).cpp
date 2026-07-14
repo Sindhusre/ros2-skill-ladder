eb5fa0ccb53187b744257c1fef4a3021389cb907
Sindhusree <162947415+Sindhusre@users.noreply.github.com>
2026-07-14 10:57:42 +0530

    Update and rename subscriber(listener) to subscriber(listener).cpp

---
+++ module1_Fundamentals/subscriber(listener).cpp
@@ -0,0 +1,24 @@
+#include<memory>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgd/msg/float64.hpp"
+
+class Batterylevel_subscriber :public rclcpp ::Node
+{
+public:
+  Batterylevel_subscriber():Node("Batterylevel_subscriber"){
+    auto topic_callback=
+       [this](std_msgd::msg::Float64::UniquePtr msg)->void{
+         RCLCPP_INFO(this->get_logger(),"the battery level is: %.2f",msg->data);
+       };
+     subscription_=
+       this->create_subscription<std_msgs::msg::Float64>("battery_level",10,topic_callback);
+    private:
+       rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr subscription_;
+};
+int main(int argc , char *argv[]){
+  rclcpp::init(argc,argv);
+  rclcpp::spin(std::make_shared<Batterylevel_subscriber>());
+  rclcpp::shutdown();
+  return 0;
+  
+}
