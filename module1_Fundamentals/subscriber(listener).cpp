bfb6c295a859c99c4b2b2e73cefc8d29e8eda453
Sindhusree <162947415+Sindhusre@users.noreply.github.com>
2026-07-14 11:10:00 +0530

    Implement battery level monitoring in subscriber
    Added battery level checks with appropriate logging for different levels.

--- module1_Fundamentals/subscriber(listener).cpp
+++ module1_Fundamentals/subscriber(listener).cpp
@@ -9,6 +9,19 @@ public:
     auto topic_callback=
        [this](std_msgd::msg::Float64::UniquePtr msg)->void{
          RCLCPP_INFO(this->get_logger(),"the battery level is: %.2f",msg->data);
+         if(msg->data>50.00){
+           RCLCPP_INFO(this->get_logger(),"the battery level is good");
+         }
+         
+         else if(msg->data>20.00){
+           RCLCPP_WARN(this->get_logger(),"please charge");
+          }
+         else if(msg->data>5.00){
+           RCLCPP_ERROR(this->get_logger(),"CRITICAL please charge");
+          }
+         else{
+           RCLCPP_ERROR(this->get_logger(),"battery is dying");
+         }
        };
      subscription_=
        this->create_subscription<std_msgs::msg::Float64>("batterylevel",10,topic_callback);
