26709b49080d5f1df066a8dc6a8e4334c5bf1283
Sindhusree <162947415+Sindhusre@users.noreply.github.com>
2026-07-14 11:14:45 +0530

    Update subscriber(listener).cpp

--- module1_Fundamentals/subscriber(listener).cpp
+++ module1_Fundamentals/subscriber(listener).cpp
@@ -1,13 +1,13 @@
 #include<memory>
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgd/msg/float64.hpp"
+#include "std_msgs/msg/float64.hpp"
 
 class Batterylevel_subscriber :public rclcpp ::Node
 {
 public:
   Batterylevel_subscriber():Node("Batterylevel_subscriber"){
     auto topic_callback=
-       [this](std_msgd::msg::Float64::UniquePtr msg)->void{
+       [this](std_msgs::msg::Float64::UniquePtr msg)->void{
          RCLCPP_INFO(this->get_logger(),"the battery level is: %.2f",msg->data);
          if(msg->data>50.00){
            RCLCPP_INFO(this->get_logger(),"the battery level is good");
@@ -24,8 +24,9 @@ public:
          }
        };
      subscription_=
-       this->create_subscription<std_msgs::msg::Float64>("batterylevel",10,topic_callback);
-    private:
+       this->create_subscription<std_msgs::msg::Float64>("battery_level",10,topic_callback);
+  }
+ private:
        rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr subscription_;
 };
 int main(int argc , char *argv[]){
