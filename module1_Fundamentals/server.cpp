08abb990f104c8825a37be89eec962876dd55fc1
Sindhusree <162947415+Sindhusre@users.noreply.github.com>
2026-07-16 03:18:56 +0530

    Fix syntax errors and add main function
    Fixed syntax errors and improved the efficiency method. Added main function to initialize node and service.

--- module1_Fundamentals/server.cpp
+++ module1_Fundamentals/server.cpp
@@ -8,13 +8,15 @@ class Server :public rclcpp::Node {
    Server():Node("Server_"){
      auto topic_callback=
        [this](std_msgs::msg::Float64::UniquePtr msg)->void{
-         auto drained +=(100-msg->data) ;
-        auto timer +=1.0;};
+         drained +=(100-msg->data) ;
+         timer +=1.0;};
      subscription_=this->create_subscription<std_msgs::msg::Float64>("battery_level",10,topic_callback);
   }
 void efficiency(const std::shared_ptr<my_robot_nodes::srv::battery_efficiency::Request>request,
                 std::shared_ptr<my_robot_nodes::srv::battery_efficiency::Response>Response){
-                     response->x=request
+                     response->x=rateofdrain();
+ RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"Incoming request \na : %s",request->a);
+ RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"sending back the request : %0.2f",response->x);
                 }
 
 private:
@@ -28,6 +30,18 @@ private:
     double drained=0.0; 
     double timer=1.0;
    };
+int main(int argc,char **argv){
+ rclcpp::init(argc,argv);
+ std::shared_ptr<rclcpp::Node>node =rclcpp::Node::make_shared("battery_efficiency_client");
+ rcl::Service<my_robot_nodes::srv::battery_efficiency>::SharedPtr service=
+ node->create_service<my_robot_nodes::srv::battery_efficiency>("add_two_ints");
+ RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"Ready to add two ints ");
+
+ rclcpp::spin(node);
+ rclcpp::shitdown();
+ 
+ 
+}
 
 
 
