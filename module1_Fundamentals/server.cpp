c7b327f3a26e2ec086eaf25ca18a20bf69117caa
Sindhusree <162947415+Sindhusre@users.noreply.github.com>
2026-07-18 23:44:37 +0530

    Refactor server class and update service handling

--- module1_Fundamentals/server.cpp
+++ module1_Fundamentals/server.cpp
@@ -5,17 +5,23 @@
 
 class Server :public rclcpp::Node {
  public:
-   Server():Node("Server_"){
+   Server():Node("battery_server"){
      auto topic_callback=
        [this](std_msgs::msg::Float64::UniquePtr msg)->void{
          drained +=(100-msg->data) ;
          timer +=1.0;};
      subscription_=this->create_subscription<std_msgs::msg::Float64>("battery_level",10,topic_callback);
   }
-void efficiency(const std::shared_ptr<my_robot_nodes::srv::battery_efficiency::Request>request,
-                std::shared_ptr<my_robot_nodes::srv::battery_efficiency::Response>Response){
+  service_=this->create_service<my_robot_nodes::srv:BatteryEfficiency>(
+    "battery_efficiency",
+     std::bind(
+       &Server::efficiency,this,std::placeholder::_1,std::placeholder::_2))
+     
+  
+ void efficiency(const std::shared_ptr<my_robot_nodes::srv::BatteryEfficiency::Request>request,
+                std::shared_ptr<my_robot_nodes::srv::battery_efficiency::Response>response){
                      response->x=rateofdrain();
- RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"Incoming request \na : %s",request->a);
+ //RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"Incoming request \na : %s",request->a);
  RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"sending back the request : %0.2f",response->x);
                 }
 
@@ -27,19 +33,17 @@ private:
       return drained/timer;
     }
     rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr subscription_;
+    rclcpp::Service<my_robot_nodes::srv::BetteryEfficiency>::SharedPtr service_;
     double drained=0.0; 
-    double timer=1.0;
+    double timer=0.0;
    };
 int main(int argc,char **argv){
  rclcpp::init(argc,argv);
- std::shared_ptr<rclcpp::Node>node =rclcpp::Node::make_shared("battery_efficiency_client");
- rcl::Service<my_robot_nodes::srv::battery_efficiency>::SharedPtr service=
- node->create_service<my_robot_nodes::srv::battery_efficiency>("add_two_ints");
- RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"Ready to add two ints ");
-
- rclcpp::spin(node);
- rclcpp::shutdown();
+ rclcpp::spin(
+   std::make_shared<Server())
  
+ rclcpp::shutdown();
+ return 0;
  
 }
 
