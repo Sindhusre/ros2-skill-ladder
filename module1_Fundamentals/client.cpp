4e357a2385d1c4361edaba481a0bec92abee3c59
Sindhusree <162947415+Sindhusre@users.noreply.github.com>
2026-07-19 00:06:22 +0530

    Fix client code for battery efficiency service

--- module1_Fundamentals/client.cpp
+++ module1_Fundamentals/client.cpp
@@ -10,17 +10,17 @@ int main(int argc,char **argv)
 {
  rclcpp::init(argc,argv);
 
-  if(argc!=1){
+  if(argc=1){
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"we need drain rate ");
     return 1;
   }
   std::shared_ptr<rclcpp::Node>node =rclcpp::Node::make_shared("battery_efficiency_client");
-  rclcpp::Client<my_robot_node::srv::batteryefficiency>::SharedPtr client=
-  node->create_client<<my_robot_node::srv::batteryefficiency>("drain_rate");
+  rclcpp::Client<my_robot_nodes::srv::batteryefficiency>::SharedPtr client=
+  node->create_client<my_robot_node::srv::batteryefficiency>("battery_efficiency");
 
-  auto request=std::make_shared<batteryefficiency::srv::batteryefficiency::Request>();
+  auto request=std::make_shared<my_robot_nodes::srv::BatteryEfficiency::Request>();
 
-  request->a="battery";
+  //request->a="battery";
   while (!client->wait_for_service(1s)){
     if(!rclcpp::ok()){
       RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),"Interrupted while waiting for the service.Exiting.");
@@ -32,7 +32,7 @@ int main(int argc,char **argv)
   if(rclcpp::spin_until_future_complete(node,result)==
      rclcpp::FutureReturnCode::SUCCESS)
   {
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"Sum: %0.2f",result.get()->x);
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"),"Sum: %.2f",result.get()->x);
   }else{
     RCLCPP_ERROR(rclcpp::get_logger("rclcpp"),"Failed to call service drain_rate");
   }
