7e481856c33f22ba2eda77d872a68e32308cc792
Sindhusree <162947415+Sindhusre@users.noreply.github.com>
2026-07-22 16:45:02 +0530

    Update life_cycle_node.cpp

--- module1_Fundamentals/life_cycle_node.cpp
+++ module1_Fundamentals/life_cycle_node.cpp
@@ -8,7 +8,7 @@ using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface
 class BatteryLifeCycle:public rclcpp_lifecycle::LifecycleNode{
   public:
       BatteryLifeCycle(): LifecycleNode("battery_lifecycle"){
-        RCL_INFO(gte_logger(),"node created");
+        RCLCPP_INFO(get_logger(),"node created");
       }
         
       CallbackReturn on_configure(const rclcpp_lifecycle::State &){
@@ -17,11 +17,11 @@ class BatteryLifeCycle:public rclcpp_lifecycle::LifecycleNode{
                      state_of_charge=msg->data;
                    };
               subscription_=
-                       this->create_subscription<std_msgs::msg::Float64>("battery_level",10,topic_callback);
+                       this->create_subscription<std_msgs::msg::Float64>("batterylevel",10,topic_callback);
   
       
         RCLCPP_INFO(get_logger(),"state of charge is =%0.2f",State_of_charge);
-        if(state_of_charge<30){
+        if(State_of_charge<30){
           RCL_INFO(get_logger(),"charge is less cannot opearte now");
           return CallbackReturn::FAILURE;}
         return CallbackReturn::SUCCESS;
@@ -34,32 +34,38 @@ CallbackReturn on_acitive(const rclcpp_lifecycle:: State &){
   
 }
 CallbackReturn on_deactivate(const rclcpp_lifecycle::State&){
-  if(state_of_charge<15){
+  if(State_of_charge<15){
     RCLCPP_INFO(get_logger(),"less power retun to home");
   }
+  return CallbackReturn::SUCCESS;
 }
 CallbackReturn on_cleanup(const rclcpp_lifecycle::State&){
   RCLCPP_INFO(get_logger(),"low power navigation");
   RCLCPP_INFO(get_logger(),"turning o fthe high power_suction motors");
-  
+  return CallbackReturn::SUCCESS;
 }
-CallbackReturn on_cleanup(const rclcpp_lifecycle::State&){
+CallbackReturn on_shutdown(const rclcpp_lifecycle::State&){
   
   RCLCPP_INFO(get_logger(),"turning o f motors");
   motor=false;
-  
+  return CallbackReturn::SUCCESS;
 }
 
 private:
 bool motor=false;
-double state_of_charge=0;
+double State_of_charge=0;
+rclcpp::Subscription
+<std_msgs::msg::Float64>
+::SharedPtr subscription_; 
 };
 int main(int argc,char*argv[])
 {
  rclcpp::init(argc,argv);
-rclcpp::spin(
-  std::make_shared<BatteryLifeCycle>());
-  rclcpp::shutdown();
+ auto lc_node = std::make_shared<BatteryLifeCycle>();
+ rclcpp::executors::SingleThreadedExecutor executor;
+ executor.add_node(lc_node->get_node_base_interface());
+ rclcpp::spin();
+ rclcpp::shutdown();
   return 0;
 }
   
